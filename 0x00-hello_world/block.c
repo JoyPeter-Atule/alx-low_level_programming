2abff9d1f2abd1a856633367f23c120983d16dea
JoyPeter-Atule <peterjoy987@gmail.com>
2022-06-30 08:47:47 -0700

    Printing a statement usig puts function

---
+++ 0x00-hello_world/block.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main:void and first entry
+ *
+ * body puts:insert statement
+ * return: set at 0
+ */
+int main(void)
+{
+    puts("Programming is like buidling a mulitlingual language\n");
+    return (0);
+}
