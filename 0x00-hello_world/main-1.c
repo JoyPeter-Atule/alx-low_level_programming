2c001747e12ab4602010da2173156170d4b90ab5
JoyPeter-Atule <peterjoy987@gmail.com>
2022-06-30 07:37:53 -0700

    how to generate an assembly code of a C code and save it in an output file

---
+++ 0x00-hello_world/main-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main:primary entry and empty
+ * return:set to 0
+ */
+int main(void)
+{
+    return (0);
+}
+
---
+++ 0x00-hello_world/main-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main: entry point and empty
+ * return:return set at 0
+ */
+int main(void)
+{
+    return (0);
+}
